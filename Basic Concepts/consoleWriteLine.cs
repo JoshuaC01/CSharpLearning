d1b93e0ef6abf560f3be406d4b108bcea6de4d2d
Joshua Cavill <josh.cavill01@gmail.com>
2019-04-17 22:27:41 +1000

    Console.WriteLine and string substitution

---
+++ Basic Concepts/consoleWriteLine.cs
@@ -0,0 +1,17 @@
+//Must include using System;
+using System;
+
+public class consoleExplanation {
+  public static void Main(string[] args)
+  {
+    //Console.WriteLine outputs to console
+    Console.WriteLine("Hello World");
+
+    //Substitute Variables into strings
+
+    string someValue = "Mono";
+
+    //Must include $ at beginning of string
+    Console.WriteLine($"Hello {someValue} World");
+  }
+}
