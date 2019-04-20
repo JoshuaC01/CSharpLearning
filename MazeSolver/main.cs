4a96c07d2a4fa4e51df87c94a409a7e015b1e8a6
JoshuaC01 <cavillj21@barker.college>
2019-04-20 23:17:02 +1000

    Maze Solver Updated. I think I understand A* but at this point no clue. The maze has been converted ino a list of junctions as desicions only need to be made there to save time.

--- MazeSolver/main.cs
+++ MazeSolver/main.cs
@@ -1,8 +1,80 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Windows.Forms;
 
-public class MazeSolver {
+public class point {
+  public int x, y, value;
+
+  public point() {
+    this.x = 0;
+    this.y = 0;
+    this.value = -1;
+  }
+
+  public point(int x, int y, int value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
+
+  public override string ToString()
+  {
+    return $"X: {this.x}, Y: {this.y}";
+  }
+}
+
+public class juncPoint {
+  public point origin;
+  public bool up, down, left, right;
+  public int distToStart, distToEnd, totalDist;
+  public List<point> cameFrom;
+
+  public juncPoint() {
+    this.up = this.down = this.left = this.right = false;
+    this.distToStart = this.distToEnd = this.totalDist = -1;
+  }
+
+  public juncPoint(point origin, bool up, bool down, bool left, bool right, point startPoint, point endPoint) {
+    this.origin = origin;
+    this.up = up;
+    this.down = down;
+    this.left = left;
+    this.right = right;
+
+    this.distToEnd = (int) (Math.Pow(endPoint.x - origin.x, 2) + Math.Pow(endPoint.y - origin.y, 2));
+    this.totalDist = int.MaxValue;
+  }
+
+  public override string ToString() {
+    String output = $"Origin({this.origin})";
+
+    output += this.up ? $", Up" : "";
+    output += this.down ? $", Down" : "";
+    output += this.left ? $", Left" : "";
+    output += this.right ? $", Right" : "";
+
+    return output;
+  }
+
+}
+
+public class MazeSolver : Form {
+
+  private PictureBox displayBox;
+  private Bitmap canvas;
+
+  private Color startColor = Color.FromArgb(0, 255, 0);
+  private Color endColor = Color.FromArgb(0, 0, 255);
+  private Color junctionColor = Color.FromArgb(255, 0, 0);
+
+  public static Color lerpColors(Color startColor, Color endColor, float t) {
+    float red = startColor.R * (1f - t) + endColor.R * t;
+    float green = startColor.G * (1f - t) + endColor.G * t;
+    float blue = startColor.B * (1f - t) + endColor.B * t;
+
+    return Color.FromArgb((int) red, (int) green, (int) blue);
+  }
 
   public static void Print2DArray<T>(T[,] matrix)
   {
@@ -16,28 +88,168 @@ public class MazeSolver {
       }
   }
 
-  public static void Main(String[] args) {
-    Bitmap mazeImg = new Bitmap("maze.png");
+  public void DisplayGUI() {
+    this.Name = "Maze Solver GUI";
+    this.Text = "Maze Solver GUI";
+    this.Size = new Size(600, 600);
+    this.StartPosition = FormStartPosition.CenterScreen;
+    this.BackColor = Color.FromArgb(200, 200, 200);
 
-    int[,] mazeArray = new int[mazeImg.Width, mazeImg.Height];
+    displayBox = new PictureBox();
+    displayBox.ClientSize = new Size(500, 500);
+    displayBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+    canvas = new Bitmap(displayBox.Width, displayBox.Height);
+
+    updateCanvas();
+
+    this.Controls.Add(displayBox);
+  }
+
+  public void updateCanvas() {
+    displayBox.Image = canvas;
+  }
+
+  public Tuple<point, point, point[,]> importImage(string imageName) {
+    Bitmap mazeImg = new Bitmap(imageName);
+
+    point[,] mazeArray = new point[mazeImg.Width, mazeImg.Height];
+
+    point startPoint, endPoint;
+    startPoint = endPoint = null;
 
     for(int x = 0; x < mazeImg.Width; x++) {
       for(int y = 0; y < mazeImg.Height; y++) {
         Color pixel = mazeImg.GetPixel(x, y);
-
         if(pixel.R == 0 && pixel.G == 0 && pixel.B == 0) {
-          mazeArray[x, y] = 0;
+          mazeArray[x, y] = new point(x, y, 0);
         } else {
-          mazeArray[x, y] = 1;
+          mazeArray[x, y] = new point(x, y, 1);
+        }
+
+        if(y == 0 && pixel.R == 255 && pixel.G == 255 && pixel.B == 255)  {
+          startPoint = mazeArray[x, y];
+        } else if(y == mazeImg.Height - 1 && pixel.R == 255 && pixel.G == 255 && pixel.B == 255)  {
+          endPoint = mazeArray[x, y];
+        }
+      }
+    }
+
+    //Print2DArray(mazeArray);
+    //displayBox.Image = mazeImg;
+
+    return Tuple.Create(startPoint, endPoint, mazeArray);
+  }
+
+  public List<juncPoint> findJunctions(point[,] mazeArray, point startPoint, point endPoint) {
+
+    List<juncPoint> junctions = new List<juncPoint>();
+
+    for(int x = 0; x < mazeArray.GetLength(0); x++) {
+      for(int y = 0; y < mazeArray.GetLength(1); y++) {
+        if(x == 0 || x == mazeArray.GetLength(0) - 1 || y == 0 || y == mazeArray.GetLength(1) - 1) {
+          continue;
+        }
+
+        if(mazeArray[x, y].value == 0) {
+          continue;
+        }
+
+        bool up, down, left, right;
+        up = down = left = right = false;
+
+        up = mazeArray[x, y - 1].value == 1;
+        down = mazeArray[x, y + 1].value == 1;
+        left = mazeArray[x - 1, y].value == 1;
+        right = mazeArray[x + 1, y].value == 1;
+
+        if((up && left) || (up && right) || (down && left) || (down && right)) {
+          junctions.Add(new juncPoint(mazeArray[x, y], up, down, left, right, startPoint, endPoint));
         }
 
       }
     }
 
-    Print2DArray(mazeArray);
+    Console.WriteLine("Found Junctions: ");
+    junctions.ForEach(Console.WriteLine);
+    Console.WriteLine("Number of Junctions: " + junctions.Count);
 
-    mazeImg.Save("NewMaze.png");
+    return junctions;
+  }
 
+  public void drawMaze(point startPoint, point endPoint, point[,] mazeArray) {
+    int width = mazeArray.GetLength(0);
+    int height = mazeArray.GetLength(1);
+
+    Console.WriteLine("Maze Size: " + width + ", " + height);
+
+    int boxScale = (int) canvas.Width / width;
+
+    Console.WriteLine("Box Scale: " + boxScale);
+
+    Graphics g = Graphics.FromImage(canvas);
+    g.Clear(Color.White);
+
+    for(int x = 0; x < width; x++) {
+      for(int y = 0; y < height; y++) {
+        if(x == startPoint.x && y == startPoint.y) {
+          g.FillRectangle(new SolidBrush(startColor), new Rectangle(x * boxScale, y * boxScale, boxScale, boxScale));
+          continue;
+        }
+
+        if(x == endPoint.x && y == endPoint.y) {
+          g.FillRectangle(new SolidBrush(endColor), new Rectangle(x * boxScale, y * boxScale, boxScale, boxScale));
+          continue;
+        }
+
+        Color brushColor = Color.White; //lerpColors(startColor, endColor, (y + 1) / (float) height);
+
+        if(mazeArray[x, y].value == 1) {
+          g.FillRectangle(new SolidBrush(brushColor), new Rectangle(x * boxScale, y * boxScale, boxScale, boxScale));
+        }
+
+        if(mazeArray[x, y].value == 0) {
+          g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(x * boxScale, y * boxScale, boxScale, boxScale));
+          continue;
+        }
+      }
+    }
+
+    List<juncPoint> junctions = findJunctions(mazeArray, startPoint, endPoint);
+
+    foreach(juncPoint junc in junctions) {
+      g.FillRectangle(new SolidBrush(junctionColor), new Rectangle(junc.origin.x * boxScale, junc.origin.y * boxScale, boxScale, boxScale));
+    }
+
+    List<juncPoint> path =  aStar(junctions, startPoint, endPoint);
+
+    g.Dispose();
+    updateCanvas();
+
+    canvas.Save("NEW - maze.png");
+  }
+
+  public List<juncPoint> aStar(List<juncPoint> allJunctions, point startPoint, point endPoint) {
+    Console.WriteLine("Starting A* Solver");
+    List<juncPoint> currentPath = new List<juncPoint>();
+
+    List<juncPoint> openPoints = new List<juncPoint>();
+    List<juncPoint> closedPoints = new List<juncPoint>();
+
+    return currentPath;
+  }
+
+  public MazeSolver() {
+    DisplayGUI();
+
+    var imageData = importImage("maze.png");
+
+    point[,] mazeArray = imageData.Item3;
+    drawMaze(imageData.Item1, imageData.Item2, mazeArray);
+  }
+
+  public static void Main(String[] args) {
+    Application.Run(new MazeSolver());
   }
 
 }
